527e2f46e6ef4d8ffb70aa85638da91e534052de
Khaia <hkarur@asu.edu>
2024-02-07 17:35:16 -0700

    Added both funstionalities sprint list and sprint stats

--- src/main/java/Sprint.java
+++ src/main/java/Sprint.java
@@ -23,7 +23,6 @@ public class Sprint {
         // Open any Taiga project and check the url of your browser. Slug name is the value after " /project/SLUG_NAME "
         // Example https://tree.taiga.io/project/SLUG_NAME/us/1?no-milestone=1
 
-        //String projectSlug = promptUser("Enter the Taiga project slug: ");
         String endpoint = TAIGA_API_ENDPOINT + "/milestones?project=" + projectId;
 
         HttpGet request = new HttpGet(endpoint);
@@ -35,7 +34,34 @@ public class Sprint {
         if (responseJson != null) {
             try {
                 JsonNode milestoneInfo = objectMapper.readTree(responseJson);
-                System.out.println(milestoneInfo);
+                //System.out.println(milestoneInfo);
+                //System.out.println("Enter the name of the Sprint selected");
+                for (JsonNode jsonObject : milestoneInfo) {
+                    //JsonNode projectExtraInfo = jsonObject.get("project_extra_info");
+        
+                    String name = jsonObject.get("name").asText();
+                    int id = jsonObject.get("id").asInt();
+        
+                    System.out.println("Name: " + name + ", ID: " + id);
+                    
+                }
+                System.out.println("Enter the name of the sprint");
+                String sprint= scanner.nextLine(); 
+                
+                for (JsonNode jsonObject : milestoneInfo) {
+                    //JsonNode projectExtraInfo = jsonObject.get("project_extra_info");
+        
+                    String name = jsonObject.get("name").asText();
+                    int id = jsonObject.get("id").asInt();
+
+                    if(sprint.equals(name)){
+                        getMilestoneStats(authToken, TAIGA_API_ENDPOINT, projectId, id);
+                    }
+        
+                    //System.out.println("Name: " + name + ", ID: " + id);
+                    
+                }
+                
                 //return milestoneInfo;
             } catch (Exception e) {
                 e.printStackTrace();
@@ -43,6 +69,30 @@ public class Sprint {
         }
 
         //return null;
+    }
+
+    public static void getMilestoneStats(String authToken,String TAIGA_API_ENDPOINT,int projectId, int milestoneId){
+        String endpoint = TAIGA_API_ENDPOINT + "/milestones/"+ milestoneId +"/stats";
+
+        HttpGet request = new HttpGet(endpoint);
+        request.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + authToken);
+        request.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+
+        // /api/v1/milestones/{milestoneId}/stats - API Call
+
+        String responseJson = HTTPRequest.sendHttpRequest(request); 
+
+        if(responseJson!=null){
+            try{
+                JsonNode milestoneStats = objectMapper.readTree(responseJson);
+                System.out.println(milestoneStats);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
     }
 }
 
