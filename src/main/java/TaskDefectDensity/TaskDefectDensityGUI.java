342aae214aad8583b3eb8f6a4890adf59f0b4372
Khaia <hkarur@asu.edu>
2024-02-27 23:27:36 -0700

    Added Task Defect Density Visualization with place holders for data to be fetched through API

--- src/main/java/TaskDefectDensity/TaskDefectDensityGUI.java
+++ src/main/java/TaskDefectDensity/TaskDefectDensityGUI.java
@@ -1,16 +1,36 @@
 package TaskDefectDensity;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.chart.PieChart;
 import javafx.stage.Stage;
 
 public class TaskDefectDensityGUI extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Task Defect Density");
+
+        ObservableList<PieChart.Data> pieChartData = getChartData();
+
+        PieChart pieChart = new PieChart(pieChartData);
+
+        Scene scene = new Scene(pieChart, 800, 600);
+        stage.setScene(scene);
+
+        stage.show();
     }
 
-    @Override
-    public void start(Stage primaryStage) {
+    private ObservableList<PieChart.Data> getChartData() {
+        //Just an example task defect density visualization - to be finished with data from API calls
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
+                new PieChart.Data("Unfinished Tasks", 30),
+                new PieChart.Data("Removed Tasks", 25),
+                new PieChart.Data("Other Tasks", 45)
+        );
 
+        return pieChartData;
     }
 }
