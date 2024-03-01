9bc33527d442b05fbf376a5ac31f3999901c365c
Ashish Thanga <ashishthanga@ashishs-mbp.mynetworksettings.com>
2024-03-01 01:42:07 -0700

    Line graph for Task Churn

--- src/main/java/TaskChurn/TaskChurnGUI.java
+++ src/main/java/TaskChurn/TaskChurnGUI.java
@@ -1,10 +1,15 @@
 package TaskChurn;
 
+
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.TreeMap;
+import javafx.scene.Scene;
 
 import javafx.application.Application;
+import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 
 public class TaskChurnGUI extends Application {
@@ -24,13 +29,37 @@ public class TaskChurnGUI extends Application {
 
     @Override
     public void start(Stage stage) {
-        stage.setTitle("Task Churn Graph for "+this.selectedSprint);
-        // Defining the axes
+        stage.setTitle("Task Churn Graph for " + selectedSprint);
         final NumberAxis xAxis = new NumberAxis();
         final NumberAxis yAxis = new NumberAxis();
-
-        // Setting labels for axes
         xAxis.setLabel("Date");
         yAxis.setLabel("Churn Percentage");
+
+        final LineChart<Number, Number> lineGraph = new LineChart<>(xAxis, yAxis);
+        lineGraph.setTitle("Task Churn");
+
+        XYChart.Series<Number, Number> series = new XYChart.Series<>();
+        series.setName(selectedSprint);
+
+        LocalDate startDate = taskChurnMap.firstKey();
+        // Add data to the series
+        taskChurnMap.forEach((date, churn) -> {
+            long daysBetween = ChronoUnit.DAYS.between(startDate, date);
+            series.getData().add(new XYChart.Data<>(daysBetween, churn));
+        });
+
+        lineGraph.getData().add(series);
+
+        xAxis.setTickLabelFormatter(new NumberAxis.DefaultFormatter(xAxis) {
+            @Override
+            public String toString(Number object) {
+                LocalDate tickDate = startDate.plusDays(object.longValue());
+                return tickDate.toString();
+            }
+        });
+
+        Scene scene = new Scene(lineGraph, 800, 600);
+        stage.setScene(scene);
+        stage.show();
     }
 }
