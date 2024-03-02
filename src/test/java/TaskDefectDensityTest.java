e3ab4d53b0a0c1f5cb4684128202dc41030c45a2
Akshat Bakliwal <abakliw1@asu.edu>
2024-03-01 19:33:47 -0500

    Added Some Test Cases

--- src/test/java/TaskDefectDensityTest.java
+++ src/test/java/TaskDefectDensityTest.java
@@ -1,23 +1,8 @@
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-import org.apache.commons.lang3.tuple.Pair;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import Authentication.Authentication;
-import utils.Tasks;
 import TaskDefectDensity.TaskDefectDensity;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static javafx.beans.binding.Bindings.when;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyString;
 
 public class TaskDefectDensityTest {
     @Test
@@ -36,7 +21,7 @@ public class TaskDefectDensityTest {
         assertEquals(3, taskDefectDensity.getNumberOfTotalTasks());
         assertEquals(0, taskDefectDensity.getNumberOfUnfinishedTasks());
         assertEquals(3, taskDefectDensity.getNumberOfDeletedTasks());
-        assertEquals(66.67, taskDefectDensity.getTaskDefectDensity(), 0.01);
+        assertEquals(100, taskDefectDensity.getTaskDefectDensity(), 0.01);
     }
 
 
