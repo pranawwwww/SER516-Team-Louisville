a57b6eb077a3cbb3197feea7b5cb41158bf23dce
ashishthanga22 <athanga@asu.edu>
2024-03-25 19:18:02 -0700

    Task inertia viz

--- src/main/java/TaskInertia/TaskInertiaGUI.java
+++ src/main/java/TaskInertia/TaskInertiaGUI.java
@@ -7,9 +7,14 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.Callback;
@@ -37,16 +42,17 @@ public class TaskInertiaGUI extends Application{
         DatePicker startDatePicker = new DatePicker();
         startDatePicker.setValue(projectStartDate); 
         startDatePicker.setDayCellFactory(getDateCellFactory(projectStartDate, LocalDate.now()));
+
         DatePicker endDatePicker = new DatePicker();
         endDatePicker.setValue(LocalDate.now()); 
         endDatePicker.setDayCellFactory(getDateCellFactory(projectStartDate, LocalDate.now()));
 
-        // Create a button to print selected dates
         Button btnSubmit = new Button("Submit");
         btnSubmit.setOnAction(e -> {
             LocalDate startDate = startDatePicker.getValue();
             LocalDate endDate = endDatePicker.getValue();
             TreeMap<LocalDate, Float> taskInertiaMap = TaskInertia.getTaskInertia(projectId, authToken, endpoint, startDate, endDate);
+            displayBarChart(taskInertiaMap, startDate, endDate, primaryStage);
         });
 
         // Layout
@@ -60,25 +66,59 @@ public class TaskInertiaGUI extends Application{
         primaryStage.show();
     }
 
+    private void displayBarChart(TreeMap<LocalDate, Float> taskInertiaMap, LocalDate startDate, LocalDate endDate, Stage stage) {
+        CategoryAxis xAxis = new CategoryAxis();
+        NumberAxis yAxis = new NumberAxis();
+        yAxis.setLabel("Task Inertia (%)");
 
-        private Callback<DatePicker, DateCell> getDateCellFactory(LocalDate minDate, LocalDate maxDate) {
-        return new Callback<>() {
-            @Override
-            public DateCell call(final DatePicker datePicker) {
-                return new DateCell() {
-                    @Override
-                    public void updateItem(LocalDate item, boolean empty) {
-                        super.updateItem(item, empty);
+        BarChart<String, Number> barChart = new BarChart<>(xAxis, yAxis);
+        barChart.setTitle("Task Inertia from " + startDate + " to " + endDate);
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+        series.setName("Inertia");
 
-                        // Disable dates before minDate or after maxDate
-                        if (item.isBefore(minDate) || item.isAfter(maxDate)) {
-                            setDisable(true);
-                            setStyle("-fx-background-color: #ffc0cb;"); // Optional: Apply a style (e.g., background color)
-                        }
-                    }
-                };
-            }
-        };
+        taskInertiaMap.forEach((date, inertia) -> {
+            XYChart.Data<String, Number> dataPoint = new XYChart.Data<>(date.toString(), inertia);
+            series.getData().add(dataPoint);
+
+            Tooltip tooltip = new Tooltip(String.format("Inertia: %.2f%%", inertia));
+            Tooltip.install(dataPoint.getNode(), tooltip);
+
+            dataPoint.nodeProperty().addListener((obs, oldNode, newNode) -> {
+                if (newNode != null) {
+                    Tooltip.install(newNode, tooltip);
+                }
+            });
+        });
+
+        barChart.getData().add(series);
+
+        VBox vBox = new VBox(barChart);
+        vBox.setAlignment(Pos.CENTER);
+        vBox.setPadding(new Insets(20));
+
+        Scene scene = new Scene(vBox, 800, 600);
+        stage.setScene(scene);
     }
+
+
+    private Callback<DatePicker, DateCell> getDateCellFactory(LocalDate minDate, LocalDate maxDate) {
+    return new Callback<>() {
+        @Override
+        public DateCell call(final DatePicker datePicker) {
+            return new DateCell() {
+                @Override
+                public void updateItem(LocalDate item, boolean empty) {
+                    super.updateItem(item, empty);
+
+                    // Disable dates before minDate or after maxDate
+                    if (item.isBefore(minDate) || item.isAfter(maxDate)) {
+                        setDisable(true);
+                        setStyle("-fx-background-color: #ffc0cb;"); // Optional: Apply a style (e.g., background color)
+                    }
+                }
+            };
+        }
+    };
+}
     
 }
